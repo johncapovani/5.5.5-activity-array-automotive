4f6289bfde0ada59c2e8a0228a9584f8192c2249
John Capovani <john@Capovani.com>
2022-09-01 23:15:10 -0400

    completed assignment

--- vehicle.js
+++ vehicle.js
@@ -62,14 +62,12 @@ class Vehicle {
         decelerate();
     }
 
-    autoPark()
-    {
+    autoPark() {
 
     }
 
-    autoDrive()
-    {
-      
+    autoDrive() {
+
     }
 
     typeOfVehicle(wheels) {
@@ -88,4 +86,50 @@ class Vehicle {
 //This exports things you want to use from this "module", more info in readme
 module.exports = {
     Vehicle
+}
+
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+
 }
